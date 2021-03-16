9de2ea68ead3ed5756431c6b90298cdcc76955ec
Richard Ashley <rashley@gmail.com>
2021-03-16 14:41:48 +0100

    fix db

--- www/meteodb.sql
+++ www/meteodb.sql
@@ -1,4 +1,13 @@
 CREATE TABLE meteolog (Date TEXT unique, hostname TEXT, temperature REAL, humidity REAL, pressure REAL, CPU REAL, IRambient REAL, IRsky REAL);
 
-CREATE TABLE meteodetail (Date TEXT unique, hostname TEXT, exttemp REAL, exthumidity REAL, extpressure REAL, inttemp REAL, intpressure REAL, inthumidity, CPU REAL);
 
+CREATE TABLE meteodetail (Date TEXT unique, hostname TEXT, exttemp REAL, exthumidity REAL, extpressure REAL, inttemp REAL, inthumidity REAL, intpressure REAL, CPU REAL);
+
+
+create table temp as select Date, hostname, exttemp, exthumidity, extpressure, inttemp, inthumidity, intpressure, CPU from meteodetail;
+
+drop table meteodetail;
+
+CREATE TABLE meteodetail (Date TEXT unique, hostname TEXT, exttemp REAL, exthumidity REAL, extpressure REAL, inttemp REAL, inthumidity REAL, intpressure REAL, CPU REAL);
+
+insert into meteodetail select Date, hostname, exttemp, exthumidity, extpressure, inttemp, intpressure, inthumidity, CPU from temp;
